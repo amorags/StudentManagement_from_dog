8100fae06a5caab49f9414cc986bc9e48d493304
oliver jorgensen <oliver.jorgensen@visma.com>
2025-03-03 16:39:55 +0100

    I don't like github

---
+++ Models/Instructor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentManagement.Models;
+
+public partial class Instructor
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public DateTime HireDate { get; set; }
+
+    public virtual ICollection<Course> Courses { get; set; } = new List<Course>();
+}
