a45b6e8ba4b26e7fc40c9956be765e08449bfbb0
oliver jorgensen <oliver.jorgensen@visma.com>
2025-03-03 14:21:23 +0100

    feat: add MiddleName to Student (Code-First)

--- Models/Student.cs
+++ Models/Student.cs
@@ -8,6 +8,7 @@ public class Student
 {
     public int Id { get; set; }
     public required string FirstName { get; set; }
+    public string? MiddleName { get; set; }  // New attribute
     public required string LastName { get; set; }
     public required string Email { get; set; }
     public DateTime EnrollmentDate { get; set; }
